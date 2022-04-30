5cdbfd07921cdada6984b08ce5a7edcf2b91b9c9
deavetheferret <francois.devantay@studentfr.ch>
2022-04-30 16:56:04 +0200

    update all value and add a background for the game

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function OpenMenuSaves() {
 function OpenMenuSettings() {
   document.getElementById('settings').classList.toggle('open')
 }
+
+function LaunchGame() {
+  document.getElementById('game-screen').classList.toggle('launched-game')
+  document.getElementById('main').classList.toggle('launched-menu')
+  document.getElementById('header').classList.toggle('launched-menu')
+  document.getElementById('body').classList.toggle('launched-body')
+}
